9fd3c259ac5ea5ea0f6d91fe2313feef0597f5d2
shireesha12 <karra.shireesha1220@gmail.com>
2018-02-13 21:05:56 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/*eslint-env browser*/
+
+/*var guess = window.prompt("guess a no 1 and 10");
+if (guess === "5") {
+    document.write("you guessed right!");
+} else {
+    document.write("you guessed wrong...!");
+}*/
+
+/*var guess = window.prompt("Guess a number between 1 and 10");
+(guess == 5) ? document.write("You guessed right!") : document.write("You guessed wrong!");*/
+
+/*var grade = window.prompt("enter the student's grade");
+if (!isNaN(grade) || grade > 100) {
+    window.document.write("either grade was not a number or grade was greater thaan 100!")
+}
+  else {  
+if (grade >= 90 && grade <= 100) {
+    window.document.write("student recieved an A.");
+} else if (grade >= 80) {
+    window.document.write("student recieved an B.");
+} else if (grade >= 70) {
+    window.document.write("student recieved an C.");
+} else if (grade >= 60) {
+    window.document.write("student recieved an D.");
+} else {
+     window.document.write("student failed the course.");
+}
+
+}*/
+
+/*var grade = window.prompt("enter the student's grade");
+switch (grade) {
+    case "A":
+        window.document.write("student is eligable to articulation with an A");
+        break;
+    case "B":
+        window.document.write("student is eligable to articulation WITH AN B");
+        break;
+    case "C":
+        window.document.write("student is not eligable to articulation");
+        break;
+        
+}*/
+
+/*var i;
+for (i = 1; i <= 10; i += 1) {
+    window.document.write(i + "<br>");
+}*/
+
+/*var x = 0;
+while (x < 10) {
+    x += 1;
+    window.document.write("I'm on number:" + String(x) + "<br>");
+}
+window.document.write("you have reached the end!");*/
+
+/*var i = 0;
+do { 
+    i += 1;
+    document.write(i + "<br>");
+} while (i < 5);
+document.write("I have reached the end!");*/
+var x = 0;
+do {
+    x += 1;
+    window.document.write("I'm on number:" + String(x) + "<br>");
+}while (x < 10);
+window.document.write("you have reached the end!");
+
